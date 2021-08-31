e4ee1af9b983b8fdd6115ccb9a2c096a80a0e133
MitarM <tarmitebra@gmail.com>
2021-08-31 19:14:13 +0200

    Dodat portal

---
+++ Portal/account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.views.generic.base import TemplateView
+
+class MyTemplateView(TemplateView):
+    pass
+
+urlpatterns = [
+
+]
+
+
+
+
