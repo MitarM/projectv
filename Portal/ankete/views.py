e3cc2d525dc4b36157bafbc445fd064d7e1e4a75
MitarM <87225366+MitarM@users.noreply.github.com>
2021-09-23 01:20:25 +0200

    Delete Portal directory

--- Portal/ankete/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse("<h1><center><em>Početak ankete</em></center></h1>")
-
-def pitanja(request, id):
-    return HttpResponse(f'Pitanje broj: {id}')
--- Portal/diskutovanje/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'diskutovanje'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:id>/', views.detaljiDiskusije, name='detaljiDiskusije'),
-]
